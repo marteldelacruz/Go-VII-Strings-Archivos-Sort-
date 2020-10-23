5ce164868317ae345bd307f68106fc6c33982ab3
Martel <martel.mosqueda@alumnos.udg.mx>
2020-10-22 19:46:27 -0500

    Se lee la lista de strings

--- main.go
+++ main.go
@@ -3,5 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Caracoles")
+	var amoutOfStrings int
+	var list []string
+
+	// get user input
+	fmt.Print("Amount of strings: ")
+	fmt.Scanln(&amoutOfStrings)
+
+	for i := 0; i < amoutOfStrings; i++ {
+		fmt.Printf("#%d: ", i+1)
+		temp := ""
+		// read new string
+		fmt.Scanln(&temp)
+
+		// save string on list
+		list = append(list, temp)
+	}
+
+	// print list
+	for _, s := range list {
+		fmt.Println(s)
+	}
 }
