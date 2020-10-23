9f8e2b24a918b88d3c9955e96ecbfbb0235c4d6a
Martel <martel.mosqueda@alumnos.udg.mx>
2020-10-22 20:48:00 -0500

    Agregada función para guardar archivo

--- main.go
+++ main.go
@@ -1,6 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"sort"
+)
+
+const (
+	ASC = "asc"
+	DES = "des"
+)
+
+func sortList(values *[]string, order string) {
+	// sort values
+
+	switch order {
+	case ASC:
+		sort.Strings(*values)
+		break
+	case DES:
+		sort.Strings(sort.Reverse(sort.StringSlice(*values)))
+		break
+	}
+}
+
+func saveToFile(values *[]string, fileName string) bool {
+	// creating file
+	file, err := os.Create(fileName)
+
+	// verify if the files has been created
+	if err != nil {
+		return false
+	}
+
+	// writing content
+	for _, s := range *values {
+		file.WriteString(s + "\n")
+	}
+
+	// close file
+	defer file.Close()
+
+	return true
+}
 
 func main() {
 	var amoutOfStrings int
@@ -21,6 +63,7 @@ func main() {
 	}
 
 	// print list
+	sortList(&list, DES)
 	for _, s := range list {
 		fmt.Println(s)
 	}
